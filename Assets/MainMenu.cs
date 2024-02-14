5a7a3c8ffed992ac7164dc404eec0c9970339340
Nikesh <42144202+cnikesh@users.noreply.github.com>
2024-02-14 16:10:21 -0500

    few more changes

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 using UnityEngine.UI;
 
 
@@ -12,22 +12,19 @@ public class MainMenu : MonoBehaviour
 {
     private string gameMode;
 
-    [System.Serializable]
-    public Dropdown m_Dropdown
+
+    public TMPro.TMP_Dropdown m_Dropdown;
     private string currentPoseName;
     // Start is called before the first frame update
     void Start()
     {
-        m_Dropdown = GetComponent<Dropdown>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Keep the current index of the Dropdown in a variable
-        m_DropdownValue = m_Dropdown.value;
-        //Change the message to say the name of the current Dropdown selection using the value
-        Console.Log(m_Dropdown.options[m_DropdownValue].text);
+        Debug.Log(m_Dropdown.value);
     }
 
     public static void onStartButtonClick()
